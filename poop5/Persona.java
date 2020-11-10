b85c8d0bcc4aff13e8b9f9780ef324698a81f4be
BRNO1509 <54178275+BRNO1509@users.noreply.github.com>
2020-11-10 17:06:50 -0600

    Update Persona.java

--- poop5/Persona.java
+++ poop5/Persona.java
@@ -16,13 +16,22 @@ public class Persona {
 
     public Persona() {
     }
-
+    /**
+     * 
+     * @param Nombre Se menicona como debe ser llamado "Nombre"
+     * @param Apellido Se menicona como debe ser llamado "Apellido"
+     * @param fNacimiento Se menicona como debe ser llamado "fNacimiento"
+     */
     public Persona(String Nombre, String Apellido, Fecha fNacimiento) {
         this.Nombre = Nombre;
         this.Apellido = Apellido;
         this.fNacimiento = fNacimiento;
     }
-
+    /**
+     * 
+     * @param Nombre Se menicona como debe ser llamado "Nombre" para una seguna persona
+     * @param Apellido Se menicona como debe ser llamado "Apellido" para una segunda persona
+     */
     public Persona(String Nombre, String Apellido) {
         this.Nombre = Nombre;
         this.Apellido = Apellido;
@@ -30,31 +39,52 @@ public class Persona {
     }
     
     
-
+    /**
+     * 
+     * @return Regresa el valor de Nombre
+     */
     public String getNombre() {
         return Nombre;
     }
-
+    /**
+     * 
+     * @param Nombre Se menicona como debe ser llamado "Nombre" para una tercera persona
+     */
     public void setNombre(String Nombre) {
         this.Nombre = Nombre;
     }
-
+    /**
+     * 
+     * @return Regresa el valor de Apellido
+     */
     public String getApellido() {
         return Apellido;
     }
-
+    /**
+     * 
+     * @param Apellido Se menicona como debe ser llamado "Apellido" para una tercera persona
+     */
     public void setApellido(String Apellido) {
         this.Apellido = Apellido;
     }
-
+    /**
+     * 
+     * @return regresa el valor de fNacimiento
+     */
     public Fecha getfNacimiento() {
         return fNacimiento;
     }
-
+    /**
+     * 
+     * @param fNacimiento Se menicona como debe ser llamado "fNacimineto" para una segunda persona
+     */
     public void setfNacimiento(Fecha fNacimiento) {
         this.fNacimiento = fNacimiento;
     }
-
+    /**
+     * 
+     * @return Regresa de modo de mensaje los valores de Nombre, Apellido y fNacimineto 
+     */
     @Override
     public String toString() {
         return "{ Persona{" + "Nombre=" + Nombre + ", Apellido=" + Apellido + ", fNacimiento=" + fNacimiento + "}, ";
