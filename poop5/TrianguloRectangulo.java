b0659661f8cc5452070cb7f566c29c1753c34a41
BRNO1509 <54178275+BRNO1509@users.noreply.github.com>
2020-11-10 17:14:06 -0600

    Update TrianguloRectangulo.java

--- poop5/TrianguloRectangulo.java
+++ poop5/TrianguloRectangulo.java
@@ -16,7 +16,15 @@ public class TrianguloRectangulo {
 
     public TrianguloRectangulo() {
     }
-
+    /**
+     * 
+     * @param co Se menciona como debe ser llamado "co"
+     * @param ca Se menciona como debe ser llamado "ca"
+     * @param hip Se menciona como debe ser llamado "hip"
+     * @param alpha Se menciona como debe ser llamado "alpha"
+     * @param betta Se menciona como debe ser llamado "betta"
+     * @param triangulo Se menciona como debe ser llamado "triangulo"
+     */
     public TrianguloRectangulo(float co, float ca, float hip, int alpha, int betta, boolean triangulo) {
         this.co = co;
         this.ca = ca;
@@ -25,55 +33,94 @@ public class TrianguloRectangulo {
         this.betta = betta;
         this.triangulo = triangulo;
     }
-
+    /**
+     * 
+     * @return Refresa el valor de co
+     */
     public float getCo() {
         return co;
     }
-
+    /**
+     * 
+     * @param co Se crea un public void para co
+     */
     public void setCo(float co) {
         this.co = co;
     }
-
+    /**
+     * 
+     * @return Regresa el valor de ca
+     */
     public float getCa() {
         return ca;
     }
-
+    /**
+     * 
+     * @param ca Se crea un public void para ca
+     */
     public void setCa(float ca) {
         this.ca = ca;
     }
-
+    /**
+     * 
+     * @return Regresa el valor de hip
+     */
     public float getHip() {
         return hip;
     }
-
+    /**
+     * 
+     * @param hip Se crea un publiv void para hip
+     */
     public void setHip(float hip) {
         this.hip = hip;
     }
-
+    /**
+     * 
+     * @return Se regresa el valor de alpha
+     */
     public int getAlpha() {
         return alpha;
     }
-
+    /**
+     * 
+     * @param alpha Se crea un public void para alpha
+     */
     public void setAlpha(int alpha) {
         this.alpha = alpha;
     }
-
+    /**
+     * 
+     * @return Regresa el valor de betta
+     */
     public int getBetta() {
         return betta;
     }
-
+    /**
+     * 
+     * @param betta Se cvrea un public void para betta
+     */
     public void setBetta(int betta) {
         this.betta = betta;
     }
-
+    /**
+     * 
+     * @return Regresa el valor de triangulo
+     */
     public boolean isTriangulo() {
         return triangulo;
     }
-
+    /**
+     * 
+     * @param triangulo Se crea un public void para triangulo
+     */
     public void setTriangulo(boolean triangulo) {
         this.triangulo = triangulo;
     }
-
+    /**
+     * 
+     * @return Regresa de modo de mensaje los valores de co, ca, hip, alpha, betta y triangulo
+     */
     @Override
     public String toString() {
         return "TrCua{" + "C.O.=" + co + ", C.A.=" + ca + ", HIP=" + hip + ", Alpha=" + alpha + ", Betta=" + betta + ", Triangulo=" + triangulo + '}';
@@ -84,4 +131,3 @@ public class TrianguloRectangulo {
         this.triangulo = triangulo;
     }
 }
-
