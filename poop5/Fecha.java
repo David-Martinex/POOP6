bca237d5fec94312c9a27aea0b7cc2115a2314c7
BRNO1509 <54178275+BRNO1509@users.noreply.github.com>
2020-11-10 16:52:03 -0600

    Update Fecha.java

--- poop5/Fecha.java
+++ poop5/Fecha.java
@@ -13,43 +13,71 @@ public class Fecha {
     private int dia;
     private int mes;
     private int anio;
-
+    /**
+     * Se dan contenido a los objetos dia, mes y anio
+     */
     public Fecha() {
         this.dia = 13;
         this.mes = 5;
         this.anio = 2001;
     }
-
+    /**
+     * 
+     * @param dia Se le indica como se debe llamar "dia"
+     * @param mes Se le indica como se debe llamar "mes"
+     * @param anio Se le indica como se debe llamar "anio"
+     */
     public Fecha(int dia, int mes, int anio) {
         this.dia = dia;
         this.mes = mes;
         this.anio = anio;
     }
-
+    /**
+     * 
+     * @return Regresa el valor de dia
+     */
     public int getDia() {
         return dia;
     }
-
+    /**
+     * 
+     * @return Regresa el valor de mes
+     */
     public int getMes() {
         return mes;
     }
-
+    /**
+     * 
+     * @return Regresa el valor de anio
+     */
     public int getAnio() {
         return anio;
     }
-
+    /**
+     * 
+     * @param dia Se crea el setDia
+     */
     public void setDia(int dia) {
         this.dia = dia;
     }
-
+    /**
+     * 
+     * @param mes Se crea el SetMes
+     */
     public void setMes(int mes) {
         this.mes = mes;
     }
-
+    /**
+     * 
+     * @param anio Se crea el setAnio
+     */
     public void setAnio(int anio) {
         this.anio = anio;
     }
-
+    /**
+     * 
+     * @return Regresa de forma de mensaje la informacion de dia, mes y anio
+     */
     @Override
     public String toString() {
         return "Fecha{" + "dia=" + dia + ", mes=" + mes + ", a\u00f1o=" + anio + '}';
